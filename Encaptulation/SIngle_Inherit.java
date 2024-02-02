ef823674417ee981ed9e20071321a0d34fea1a34
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-02-03 01:23:51 +0600

    Add files via upload

---
+++ Encaptulation/SIngle_Inherit.java
@@ -0,0 +1,46 @@
+package inherit;
+
+class Father {
+    String name;
+    int age;
+
+    int Area(int x, int y) {
+        return x * y;
+    }
+}
+
+class Child extends Father {
+    Child(){
+        name = "Rabbi";
+        age = 22;
+        int x = Area(4, 6);
+        System.out.println("Area: " + x);
+    }
+}
+
+public class SIngle_Inherit {
+    public static void main(String[] args) {
+        Child ob = new Child();
+        System.out.println("Name: " + ob.name);
+        System.out.println("Age: " + ob.age);
+        
+    }
+}
+
+//class Basic{
+// int salary=43000;
+// String c_name="Enosis";
+//
+// String message(){
+//    return ("This is default method");
+// }
+//}
+//
+//class Employee extends Basic{
+//   public static void main(String [] args){
+//     //Basic b = new Basic();
+//     Employee m = new Employee (); 
+//     System.out.println("The basic salary is:"+m.salary);
+//     System.out.println(m.message());
+//   }
+//}
